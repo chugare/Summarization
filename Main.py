0bbeeda3d16e7df15469b456388c2bf65c0d8136
root <chugare@126.com>
2019-04-12 12:20:17 +0800

    lda model

---
+++ Main.py
@@ -0,0 +1,16 @@
+import Tool
+import LDA
+
+def run_task(meta):
+    taskName = meta['NAME']
+
+
+
+
+LDA_TRAIN={
+    'NAME':'LDA_TRAIN',
+    'SOURCE_FILE':'data.txt',
+    'NAME':,
+    'NAME':,
+    'NAME':,
+}
