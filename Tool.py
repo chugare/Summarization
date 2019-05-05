c3ab8e2851aca146e16d360d30a2bff457a02679
chugare <chugare@126.com>
2019-05-05 14:26:02 +0800

    Model can run now, try to train it

--- Tool.py
+++ Tool.py
@@ -221,6 +221,8 @@ if __name__ == '__main__':
     mod = arg[1]
     fileName = arg[2]
     if mod == '-t':
-        TXT2TXT_extract(fileName,"DP_comma",testCase = -1)
+        TXT2TXT_extract(fileName,"DP_comma",testCase = -1
+
+                        )
     elif mod == '-x':
         XML2TXT_extract(fileName)
