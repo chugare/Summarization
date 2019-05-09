a6b7faf868a1da7647f1573074f4adf6fee1f08a
chugare <chugare@126.com>
2019-05-09 22:57:18 +0800

    Model can run now, try to train it

--- WorkList.py
+++ WorkList.py
@@ -1,9 +1,16 @@
 from DataPipe import DataPipe
 from Meta import  Meta
-import Main
+import Tool,DataPipe,Seq2seq_hierarchical_softmax
+
+Tool.TXT2TXT_extract('../DP/rating_2.txt',"DP_comma",testCase = -1)
 meta  = Meta( ReadNum= 800000 ).get_meta()
-dp = DataPipe(**meta)
-# meta = getmeta(**meta)
-dp.write_TFRecord(meta,6)
-meta = Meta().get_meta()
-Main.run_train_task(**meta)
+dc = DataPipe.DictFreqThreshhold(**meta)
+dc.HuffmanEncoding(**meta)
+dc.getHuffmanDict()
+meta = Meta(TaskName = 'DP_s2s_hierarchacal',BatchSize = 128 ,ReadNum = 800000,
+                         LearningRate = 0.01,
+                         SourceFile='DP_comma.txt',
+                         DictName = "DP_comma_DICT.txt").get_meta()
+
+main = Seq2seq_hierarchical_softmax.Main()
+main.run_train(**meta)
