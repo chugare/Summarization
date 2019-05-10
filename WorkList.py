0284a5ce7dd3a913ac1c5065611e11fe97e06cd3
chugare <chugare@126.com>
2019-05-10 13:44:27 +0800

    Model can run now, try to train it

--- WorkList.py
+++ WorkList.py
@@ -7,6 +7,7 @@ meta  = Meta( ReadNum= 800000 ).get_meta()
 dc = DataPipe.DictFreqThreshhold(**meta)
 # dc.HuffmanEncoding(**meta)
 dc.getHuffmanDict()
+tfidf = Tool.Tf_idf(dic='DP_comma_DICT.txt',doc_file='DP_comma.txt')
 meta = Meta(TaskName = 'DP_s2s_hierarchacal',BatchSize = 128 ,ReadNum = 800000,
                          LearningRate = 0.005,
                          SourceFile='DP_comma.txt',
