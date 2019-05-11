6c2833a8087cb39af0914b6fc8b7c8b36315559f
root <chugare@126.com>
2019-05-11 13:47:43 +0800

    model

--- Seq2seq_hierarchical_softmax.py
+++ Seq2seq_hierarchical_softmax.py
@@ -577,5 +577,5 @@ if __name__ == '__main__':
                          DictName = "DP_comma_DICT.txt").get_meta()
         dp = Data(**meta).pipe_data()
         for i in dp:
-            print([len(j) for j in i])
+            print("%d %d %d %d"%(len(i[0]),len(i[1]),len(i[2]),i[3]))
         # Main().run_train(**meta)
