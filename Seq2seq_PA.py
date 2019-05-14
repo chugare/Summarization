180ba814d845ace1e9aa337b585a153e58620b46
root <chugare@126.com>
2019-05-14 10:55:37 +0800

    model

--- Seq2seq_PA.py
+++ Seq2seq_PA.py
@@ -302,6 +302,7 @@ class Model:
             maskTa = tf.TensorArray(dtype=tf.float32, size=self.MaxSentenceLength, dynamic_size=False,
                                     clear_after_read=False, tensor_array_name='Mask_ta')
 
+            PA_weight = 1/PA_weight
             ProbDecay = preWord * PA_weight
             ProbDecay = tf.squeeze(ProbDecay)
             _,new_state,outputTA,maskTa =  loopOpt(0,runState,outputTA,maskTa)
@@ -310,7 +311,7 @@ class Model:
             res = tf.tensordot(outputTensor,outWeight,[-1,0])
             # new_output = new_output*mask
             res = tf.squeeze(res)
-            res = res + ProbDecay*(-0.5)
+            res = res + ProbDecay*(0.5)
             resProb = tf.nn.softmax(res)
             ops = {
                 'SentenceVector': SentenceVector,
