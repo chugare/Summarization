e09540a936f4c86dddce407221503e89470c9f80
chugare <chugare@126.com>
2019-10-29 17:00:36 +0800

    原始数据的处理和统计模块单独成类

---
+++ data/process_data.py
@@ -0,0 +1,4 @@
+from data_util.data_source import TXT2TXT_extract
+
+TXT2TXT_extract()
+
--- data_util/data_source.py
+++ data_util/data_source.py
@@ -15,7 +15,87 @@ def TXT2TXT_extract(sourceFile, TaskName, dis_file=None,min_line = 50,
     dic = {}
     dic_pos = {}
 
-    def cut_without_comma(commentLine):
+
+
+
+
+class DatasetBuilder():
+    '''
+    为了应对不同格式的数据集的创建需求
+
+    数据集创建的基本需求是便利数据来源
+
+    不同的构建方式需要记录不同的数据
+
+    '''
+    def __init__(self,sourceFile, TaskName, dis_file=None,min_line = 50,
+                    evalSize=1000,threshold = 5):
+
+        self.sourceFile = open(sourceFile, 'r', encoding='utf-8')
+        self.TaskName = TaskName
+        self.evalSize = evalSize
+        self.threshold = threshold
+        if dis_file == None:
+            dis_file = TaskName + ".txt"
+
+        self.min_line = min_line
+        self.dis_file = dis_file
+        self.length_map = {}
+        self.dic = {}
+        self.dic_pos = {}
+        pass
+
+    def read(self):
+        countFile = 0
+
+        data_file = open(self.dis_file, 'w', encoding='utf-8')
+        eval_file = open('E_' + self.dis_file, 'w', encoding='utf-8')
+
+        for line in self.sourceFile:
+            line = line.strip()
+            # if len(line) != 0:
+            #     commentLine += line
+            # else:
+            #     if len(commentLine)!=0:
+            #         try:
+            if countFile >= 1000000:
+                break
+            commentLine = line
+            commentLine = commentLine.replace('\n', '')
+            if len(commentLine) < self.min_line:
+                continue
+            countFile += 1
+            if (countFile) % 1000 == 0:
+                print("[INFO] Now reading Line : %d " % (countFile))
+            if self.ecount < 1000:
+                self.ecount += 1
+                eval_file.write(self.cut_with_comma(commentLine))
+                eval_file.write('\n')
+            else:
+                data_file.write(self.cut_with_comma(commentLine))
+                data_file.write('\n')
+
+        dic_file = open(self.TaskName + '_DICT.txt', 'w', encoding='utf-8')
+        # pos_file = open('POS.txt','w',encoding='utf-8')
+        count = 0
+        ULSW = ['\n', '\t', ' ', '']
+        for i in ULSW:
+            self.dic[i] = 0
+        for w in self.dic:
+            if self.dic[w] > self.threshold:
+                word_type = max(self.dic_pos[w], key=lambda x: self.dic_pos[w][x])
+                wordCount = self.dic[w]
+                dic_file.write("%d %s %s %d\n" % (count, w, word_type, wordCount))
+                count += 1
+        dic_file.close()
+        # pos_file.close()
+        print("[INFO] 点评文本读取完毕 共计%d 文本 句子长度统计如下" % count)
+        # for kv in enumerate(length_map):
+        ll = sorted(self.length_map.keys(), key=lambda x: x)
+        for k in ll:
+            print("k = %d : %d" % (k, self.length_map[k]))
+
+    def cut_without_comma(self,commentLine):
         commentLine = commentLine.replace('\n', '')
         sens = re.split(r"[,、，。；：\n]", commentLine)
         patterns = [
@@ -34,23 +114,23 @@ def TXT2TXT_extract(sourceFile, TaskName, dis_file=None,min_line = 50,
             for w in cutres:
                 wc = w.word
                 wf = w.flag
-                if wc not in dic:
-                    dic[wc] = 0
-                    dic_pos[wc] = {}
-                dic[wc] += 1
-                if wf not in dic_pos[wc]:
-                    dic_pos[wc][wf] = 0
-                dic_pos[wc][wf] += 1
+                if wc not in self.dic:
+                    self.dic[wc] = 0
+                    self.dic_pos[wc] = {}
+                self.dic[wc] += 1
+                if wf not in self.dic_pos[wc]:
+                    self.dic_pos[wc][wf] = 0
+                    self.dic_pos[wc][wf] += 1
             lc = len(cutres)
-            if lc not in length_map:
-                length_map[lc] = 0
-            length_map[lc] += 1
+            if lc not in self.length_map:
+                self.length_map[lc] = 0
+            self.length_map[lc] += 1
             cutres = list(zip(*cutres))
             sen = ' '.join(list(cutres[0]))
             res.append(sen)
             return ' '.join(res)
 
-    def cut_with_comma(commentLine):
+    def cut_with_comma(self,commentLine):
 
         patterns = [
             r"[（\(]+[一二三四五六七八九十\d]+[\)）]+[，、。．,\s]*",
@@ -65,23 +145,23 @@ def TXT2TXT_extract(sourceFile, TaskName, dis_file=None,min_line = 50,
             for w in cutres:
                 wc = w.word
                 wf = w.flag
-                if wc not in dic:
-                    dic[wc] = 0
-                    dic_pos[wc] = {}
-                dic[wc] += 1
-                if wf not in dic_pos[wc]:
-                    dic_pos[wc][wf] = 0
-                dic_pos[wc][wf] += 1
+                if wc not in self.dic:
+                    self.dic[wc] = 0
+                    self.dic_pos[wc] = {}
+                self.dic[wc] += 1
+                if wf not in self.dic_pos[wc]:
+                    self.dic_pos[wc][wf] = 0
+                self.dic_pos[wc][wf] += 1
             lc = len(cutres)
-            if lc not in length_map:
-                length_map[lc] = 0
-            length_map[lc] += 1
+            if lc not in self.length_map:
+                self.length_map[lc] = 0
+            self.length_map[lc] += 1
             cutres = list(zip(*cutres))
             return ' '.join(list(cutres[0]))
 
     ecount = 0
 
-    def cut_with_comma_sen(commentLine):
+    def cut_with_comma_sen(self,commentLine):
 
         patterns = [
             r"[（\(]+[一二三四五六七八九十\d]+[\)）]+[，、。．,\s]*",
@@ -98,64 +178,17 @@ def TXT2TXT_extract(sourceFile, TaskName, dis_file=None,min_line = 50,
             for w in cutres:
                 wc = w.word
                 wf = w.flag
-                if wc not in dic:
-                    dic[wc] = 0
-                    dic_pos[wc] = {}
-                dic[wc] += 1
-                if wf not in dic_pos[wc]:
-                    dic_pos[wc][wf] = 0
-                dic_pos[wc][wf] += 1
+                if wc not in self.dic:
+                    self.dic[wc] = 0
+                    self.dic_pos[wc] = {}
+                self.dic[wc] += 1
+                if wf not in self.dic_pos[wc]:
+                    self.dic_pos[wc][wf] = 0
+                self.dic_pos[wc][wf] += 1
             lc = len(cutres)
-            if lc not in length_map:
-                length_map[lc] = 0
-            length_map[lc] += 1
+            if lc not in self.length_map:
+                self.length_map[lc] = 0
+            self.length_map[lc] += 1
             cutres = list(zip(*cutres))
             res.append(' '.join(list(cutres[0])))
         return '\n'.join(res)
-
-
-    for line in sourceFile:
-        line = line.strip()
-        # if len(line) != 0:
-        #     commentLine += line
-        # else:
-        #     if len(commentLine)!=0:
-        #         try:
-        if countFile >= 1000000:
-            break
-        commentLine = line
-        commentLine = commentLine.replace('\n', '')
-        if len(commentLine) < min_line:
-            continue
-        countFile += 1
-        if (countFile) % 1000 == 0:
-            print("[INFO] Now reading Line : %d " % (countFile))
-        if ecount < 1000:
-            ecount += 1
-            eval_file.write(cut_with_comma(commentLine))
-            eval_file.write('\n')
-        else:
-            data_file.write(cut_with_comma(commentLine))
-            data_file.write('\n')
-            #     except StopIteration:
-            #         pass
-            # commentLine = ""
-    dic_file = open(TaskName + '_DICT.txt', 'w', encoding='utf-8')
-    # pos_file = open('POS.txt','w',encoding='utf-8')
-    count = 0
-    ULSW = ['\n', '\t', ' ', '']
-    for i in ULSW:
-        dic[i] = 0
-    for w in dic:
-        if dic[w] > threshold:
-            word_type = max(dic_pos[w], key=lambda x: dic_pos[w][x])
-            wordCount = dic[w]
-            dic_file.write("%d %s %s %d\n" % (count, w, word_type, wordCount))
-            count += 1
-    dic_file.close()
-    # pos_file.close()
-    print("[INFO] 点评文本读取完毕 共计%d 文本 句子长度统计如下" % count)
-    # for kv in enumerate(length_map):
-    ll = sorted(length_map.keys(), key=lambda x: x)
-    for k in ll:
-        print("k = %d : %d" % (k, length_map[k]))
